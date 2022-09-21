0cbce6f6dbd8d8135601ddf580d30c1fe3d722aa
Marco  Rodrigues <prmarcofortal@gmail.com>
2022-09-21 20:12:00 -0300

    Tdd testes unitários .NetCore

---
+++ NewTalentsMarco/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTalentsMarco
+{
+    public class Calculadora
+    {
+        private List<string> listahistorico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+            listahistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+                return res;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public int mutiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public int dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            listahistorico.RemoveRange(3, listahistorico.Count - 3);
+            return listahistorico;
+        }
+    }
+}
